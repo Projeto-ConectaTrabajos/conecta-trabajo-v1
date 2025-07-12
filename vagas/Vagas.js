0fda0b2b61b8752217e8719bd4dfa861435a8ab3
Danielle1157 <Daniellesantos96@outlook.com>
2025-07-12 20:20:45 -0300

    Add files via upload

---
+++ vagas/Vagas.js
@@ -0,0 +1,120 @@
+function LeiaMais1(){
+    var pontos=document.getElementById("pontos");
+    var maisTexto=document.getElementById("mais");
+    var btnLeiaMais=document.getElementById("btnLeiaMais");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display ="inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+    }else{
+        pontos.style.display ="none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos"
+    }
+}
+function LeiaMais2(){
+    var pontos=document.getElementById("pontos2");
+    var maisTexto=document.getElementById("mais2");
+    var btnLeiaMais=document.getElementById("btnLeiaMais2");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display ="inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+    }else{
+        pontos.style.display ="none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos"
+    }
+}
+function LeiaMais3(){
+    var pontos=document.getElementById("pontos3");
+    var maisTexto=document.getElementById("mais3");
+    var btnLeiaMais=document.getElementById("btnLeiaMais3");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display ="inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+    }else{
+        pontos.style.display ="none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos"
+    }
+}
+function LeiaMais4(){
+    var pontos=document.getElementById("pontos4");
+    var maisTexto=document.getElementById("mais4");
+    var btnLeiaMais=document.getElementById("btnLeiaMais4");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display ="inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+    }else{
+        pontos.style.display ="none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos"
+    }
+}
+function LeiaMais5(){
+    var pontos=document.getElementById("pontos5");
+    var maisTexto=document.getElementById("mais5");
+    var btnLeiaMais=document.getElementById("btnLeiaMais5");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display ="inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+    }else{
+        pontos.style.display ="none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos"
+    }
+}
+function LeiaMais6(){
+    var pontos=document.getElementById("pontos6");
+    var maisTexto=document.getElementById("mais6");
+    var btnLeiaMais=document.getElementById("btnLeiaMais6");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display ="inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+    }else{
+        pontos.style.display ="none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos"
+    }
+}
+function LeiaMais7(){
+    var pontos=document.getElementById("pontos7");
+    var maisTexto=document.getElementById("mais7");
+    var btnLeiaMais=document.getElementById("btnLeiaMais7");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display ="inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+    }else{
+        pontos.style.display ="none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos"
+    }
+}
+function LeiaMais8(){
+    var pontos=document.getElementById("pontos8");
+    var maisTexto=document.getElementById("mais8");
+    var btnLeiaMais=document.getElementById("btnLeiaMais8");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display ="inline";
+        maisTexto.style.display = "none";
+        btnLeiaMais.innerHTML = "Leia Mais";
+    }else{
+        pontos.style.display ="none";
+        maisTexto.style.display = "inline";
+        btnLeiaMais.innerHTML = "Leia Menos"
+    }
+}
